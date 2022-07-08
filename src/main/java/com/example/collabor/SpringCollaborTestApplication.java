21b9805a09f9b4d55e3212a65641276fa427ed5c
onesakku <sakku1044@gmail.com>
2022-07-08 11:49:51 +0900

    project init

---
+++ src/main/java/com/example/collabor/SpringCollaborTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.collabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCollaborTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCollaborTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/collabor/SpringCollaborTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCollaborTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
